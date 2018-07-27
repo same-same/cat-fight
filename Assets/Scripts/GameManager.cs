8a55c14fbada6e074a76009858177126c9eedc93
murog <guillermina.muro@gmail.com>
2018-07-27 14:57:42 -0700

    moved methods to gm

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,11 +8,17 @@ namespace CatFight
 	{
 		public static float score;
 		public static int difficulty;
+		private ArrayList sequences;
+		private KeyCode[] currentSequence;
+		private int currentLength;
 		// Use this for initialization
 		void Start ()
 		{
 			score = 0.0f;
 			difficulty = 0;
+			sequences = new ArrayList ();
+			KeyCode[] sequence1 = new KeyCode[]{KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D};
+			sequences.Add(sequence1);
 		}
 	
 		// Update is called once per frame
@@ -20,6 +26,26 @@ namespace CatFight
 		{
 		
 		}
+
+		//TODO
+		private KeyCode[] generateSequence () {
+			KeyCode[] availKeyCodes = sequences [GameManager.difficulty];
+
+			if (GameManager.difficulty == 0) {
+				currentLength = 4;
+			} else {
+				//TODO
+			}
+			KeyCode[] sequence = new KeyCode[currentLength];
+			for (int i = 0; i < currentLength; i++) {
+				int index = Random.Range (1, currentLength + 1);
+				sequence [i] = availKeyCodes [index];
+			}
+			return sequence;
+
+		}
+
+
 		
 	}
 }
--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -13,13 +13,7 @@ namespace CatFight
 		private KeyCode dkey;
 		private KeyCode start;
 		private KeyCode exit;
-		private ArrayList sequences;
 		private ArrayList currentInput;
-		private KeyCode[] currentSequence;
-		private int currentLength;
-
-
-
 
 		// Use this for initialization
 		private void Start ()
@@ -27,34 +21,9 @@ namespace CatFight
 			wKey = KeyCode.W;
 			aKey = KeyCode.A;
 			sKey = KeyCode.S;
-			dkey = KeyCode.D;
-			sequences = new ArrayList ();
-			KeyCode[] sequence1 = new KeyCode[]{KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D};
-			sequences.Add(sequence1);
-
-		
+			dkey = KeyCode.D;		
 		}
-
-		//TODO
-		private KeyCode[] generateSequence () {
-			KeyCode[] availKeyCodes = sequences [GameManager.difficulty];
-
-			if (GameManager.difficulty == 0) {
-				currentLength = 4;
-			} else {
-				//TODO
-			}
-			KeyCode[] sequence = new KeyCode[currentLength];
-			for (int i = 0; i < currentLength; i++) {
-				int index = Random.Range (1, currentLength + 1);
-
-
-
-			}
-		
 			
-			
-		}
 	
 		// Update is called once per frame
 		void Update ()
